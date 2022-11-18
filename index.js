5ca0ee17478444d55966dc7eb2c019c892988695
prodip763 <prodip12763@gmail.com>
2022-11-19 00:13:23 +0600

    appointment-deshboard

--- index.js
+++ index.js
@@ -17,28 +17,73 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('booking');
 
-        app.get('/service', async(req, res)=>{
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            res.send({ success: true, result });
+        })
+
+
+        //warning:
+        //This is not the proper way to query
+        //After learning more abour mongodb. use aggregate lookup, pipline, match, group
+        app.get('/available', async (req, res) => {
+            const date = req.body.date;
+            //step 1: get all service
+            const services = await serviceCollection.find().toArray();
+
+            //step 2: get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            //step 3: for each service , 
+            services.forEach(service => {
+                // step 4: find bookings for that service
+                const serviceBookings = bookings.filter(b => b.treatment === service.name);
+                const booked = serviceBookings.map(s => s.slot);
+                // service.booked =booked; 
+                // service.booked = serviceBookings.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.slots = available;
+
+            })
+            res.send(services);
+        })
+        app.get('/booking', async(req, res)=>{
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find().toArray();
+            res.send(bookings);
+        })
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send('Hello Doctor');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Doctor App listening on por ${port}`)
 
 })
